285c324d21192a640a9578cae9efef6b96ec920e
Caiquekola <caiquekola@hptmail.com>
2024-08-04 21:48:03 -0300

    Projeto final 1.2

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-        window.location.href = '/Prototipo/home.html'; // Redireciona para index.html
+        window.location.href = '/assets/home.html'; // Redireciona para index.html
     }, 5000); // 5 segundos de tela splash
 });
