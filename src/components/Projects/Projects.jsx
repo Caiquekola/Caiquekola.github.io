e1e49b88dbe8026baf9e7cf0ad17b9584dfbd9e3
caiquekola <caiquekola@hotmail.com>
2025-05-05 18:23:41 -0300

    feature: projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -23,70 +23,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={"https://private-user-images.githubusercontent.com/99914098/414872643-e4a323b8-21c2-470f-bc2c-69ebbcc691aa.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDY0NzkwNDYsIm5iZiI6MTc0NjQ3ODc0NiwicGF0aCI6Ii85OTkxNDA5OC80MTQ4NzI2NDMtZTRhMzIzYjgtMjFjMi00NzBmLWJjMmMtNjllYmJjYzY5MWFhLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA1MDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNTA1VDIwNTkwNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQ1MWYxNmM3ODExNDM2NDhiNDA3MTlkOTZlMjQ4YTAwM2VmMzVmMGRlY2FiNmQ2ZDk0MTU4NDE0NTNlZGQ1YzAmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.8510IdEf-GGM7E-TWAy52VBxFT0-PKRFjN0iR7lD-dc"}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Virtual Terminal"
+              description="A virtual system that simulates a console and file explorer, designed to help users learn and practice Linux terminal commands"
+              ghLink="https://github.com/Caiquekola/VirtualTerminal"
+              demoLink="https://osvirtualterminal.netlify.app"  
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={"https://raw.githubusercontent.com/Caiquekola/app-barber-frontend/refs/heads/main/src/assets/HomePage.png?token=GHSAT0AAAAAAC5R7RNYA5RD6QFHVZJ6J5L22AZHEZQ"}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Ricardo Barber Shop"
+              description="A virtual system that simulates a console and file explorer, designed to help users learn and practice Linux terminal commands"
+              ghLink="https://github.com/Caiquekola/app-barber-frontend"
+              demoLink="https://ricardobarbearia.netlify.app/"  
             />
-          </Col>
-
-          <Col md={4} className="project-card">
+          </Col>+<Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={"https://private-user-images.githubusercontent.com/99914098/380557257-0e5d3fe6-a0ae-49ac-895b-b86546f73a2d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDY0ODAzMzgsIm5iZiI6MTc0NjQ4MDAzOCwicGF0aCI6Ii85OTkxNDA5OC8zODA1NTcyNTctMGU1ZDNmZTYtYTBhZS00OWFjLTg5NWItYjg2NTQ2ZjczYTJkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA1MDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNTA1VDIxMjAzOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJkY2E1NWRhNmM3MmE0YzdhMGUyNTIyMzdmZWJiYWUzMzhkMWY4ZmI2OTc3Yzg1NzkwNjkxMjQxNTA4NWQwZGYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.q-LzXG8ul7FqLiopSMbgrHHtE_Yvz2NAkI3m4Rjwc2E"}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Decifra"
+              description="A virtual system that simulates a console and file explorer, designed to help users learn and practice Linux terminal commands"
+              ghLink="https://github.com/Caiquekola/DECIFRA"
+              //demoLink="https://ricardobarbearia.netlify.app/"  
             />
           </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
+          
         </Row>
       </Container>
     </Container>
